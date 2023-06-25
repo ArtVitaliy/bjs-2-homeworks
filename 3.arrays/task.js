b1a3a5688209e37f93bd3de0b466574f3172149f
ArtVitaly <0331116@mail.ru>
2023-06-25 22:20:20 +0300

    Задача 1,2 Arrays решены

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,23 @@
 function compareArrays(arr1, arr2) {
-  
+	// Проверяем, имеют ли массивы одинаковую длину
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+
+	// Сравниваем значения элементов массивов на соответствующих индексах
+	return arr1.every((value, index) => value === arr2[index]);
+
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	const filteredUsers = users.filter(user => user.gender === gender);
+
+	if (filteredUsers.length === 0) {
+		return 0;
+	}
+
+	const totalAge = filteredUsers.reduce((sum, user) => sum + user.age, 0);
+	const averageAge = totalAge / filteredUsers.length;
+
+	return averageAge;
 }
