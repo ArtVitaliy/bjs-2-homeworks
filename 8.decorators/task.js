300dff08485414f118d0a83800c84c93de1d3720
ArtVitaly <0331116@mail.ru>
2023-08-01 23:16:09 +0300

    Задание 8 'Декораторы' выполнено

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,61 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+	let cache = [];
+
+	function wrapper(...args) {
+		const hash = md5(args);
+		const objectInCache = cache.find((item) => item.hash === hash);
+
+		if (objectInCache) { // если элемент найден в кеше
+			console.log("Из кэша: " + objectInCache.value);
+			return "Из кэша: " + objectInCache.value;
+		} else {
+			const value = func(...args);
+			cache.push({
+				hash,
+				value
+			});
+
+			if (cache.length > 5) {
+				cache.shift(); // удаляем первый объект из кеша, если его размер превышает 5
+			}
+			return "Вычисляем: " + value;
+		}
+	}
+	return wrapper;
 }
+  
+
+
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+	let timeoutId = null;
+
+	function wrapper(...args) {
+		wrapper.allCount++;
+
+		if (timeoutId) {
+			clearTimeout(timeoutId);
+		}
+
+		if (!timeoutId) {
+			wrapper.count++;
+			func(args);
+			// func.apply(this, args);
+		}
+		timeoutId = setTimeout(() => {
+			wrapper.count++;
+			func(args);
+			// func.apply(this, args);
+		}, delay);
+	}
+
+	wrapper.count = 0;
+	wrapper.allCount = 0;
+
+	return wrapper;
+
 }
+
+
